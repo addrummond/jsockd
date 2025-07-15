e9039a29e8648747832e315a75f82347b9f6de30
Alex Drummond <a.d.drummond@gmail.com>
2025-07-15 22:53:02 +0100

    initial work on downloading js_server binaries in mix.exs

--- clients/elixir/jsockd_client/mix.exs
+++ clients/elixir/jsockd_client/mix.exs
@@ -1,13 +1,19 @@
 defmodule JsockdClient.MixProject do
   use Mix.Project
 
+  @jsockd_version "0.0.6"
+
   def project do
     [
       app: :jsockd_client,
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: Mix.compilers() ++ [:download_js_server_binary],
+      aliases: [
+        "compile.download_js_server_binary": &download_js_server_binary/1
+      ]
     ]
   end
 
@@ -28,8 +34,123 @@ defmodule JsockdClient.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_hexpm, "~> 0.3.0"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp download_js_server_binary(_) do
+    release_url = get_release_url()
+
+    ensure_app!(:inets)
+    ensure_app!(:ssl)
+
+    priv_dir = Path.join(__DIR__, "priv")
+    File.mkdir_p!(priv_dir)
+    release_filename = Path.basename(release_url)
+    release_path = Path.join(priv_dir, release_filename)
+
+    unless File.exists?(Path.join([priv_dir, "release_artifacts", "js_server"])) and
+             File.exists?(
+               Path.join([
+                 priv_dir,
+                 "jsockd_js_server_version_tag_#{@jsockd_version}"
+               ])
+             ) do
+      # https://security.erlef.org/secure_coding_and_deployment_hardening/inets
+      # https://github.com/phoenixframework/phoenix/blob/a106791ea9ee94b4f1e9286e45a319794b3838e2/lib/mix/tasks/phx.gen.release.ex#L334
+      http_options = [
+        ssl: [
+          verify: :verify_peer,
+          cacerts: :public_key.cacerts_get(),
+          depth: 3,
+          customize_hostname_check: [
+            match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+          ],
+          versions: protocol_versions()
+        ]
+      ]
+
+      {:ok, {_, _, body}} =
+        :httpc.request(:get, {release_url, []}, http_options, body_format: :binary)
+
+      File.write!(release_path, body)
+
+      :ok =
+        :erl_tar.extract(String.to_charlist(release_path), [
+          {:cwd, String.to_charlist(priv_dir)},
+          :compressed
+        ])
+
+      File.rm!(release_path)
+
+      File.chmod!(
+        Path.join([
+          priv_dir,
+          "release-artifacts",
+          String.replace(release_filename, ".tar.gz", ""),
+          "js_server"
+        ]),
+        0o500
+      )
+
+      File.rename!(
+        Path.join([
+          priv_dir,
+          "release-artifacts",
+          String.replace(release_filename, ".tar.gz", "")
+        ]),
+        Path.join([
+          priv_dir,
+          "release-artifacts",
+          "jsockd"
+        ])
+      )
+
+      File.rename!(
+        Path.join([priv_dir, "release-artifacts"]),
+        Path.join([priv_dir, "jsockd-release-artifacts"])
+      )
+
+      File.touch!(Path.join([priv_dir, "jsockd_js_server_version_tag_#{@jsockd_version}"]))
+    end
+
+    :ok
+  end
+
+  defp get_release_url do
+    platform = :erlang.system_info(:system_architecture) |> to_string()
+
+    release_file =
+      cond do
+        String.contains?(platform, "x86_64") and String.contains?(platform, "linux") ->
+          "jsockd-linux-x86_64.tar.gz"
+
+        String.contains?(platform, "aarch64") and String.contains?(platform, "linux") ->
+          "jsockd-linux-arm64.tar.gz"
+
+        String.contains?(platform, "apple-darwin") and String.contains?(platform, "aarch64") ->
+          "jsockd-macos-arm64.tar.gz"
+
+        true ->
+          raise "Unsupported platform: #{platform}. Supported platforms are x86_64 and arm64 on Linux, and arm64 on MacOS."
+      end
+
+    "https://github.com/addrummond/jsockd/releases/download/v#{@jsockd_version}/#{release_file}"
+  end
+
+  # https://github.com/phoenixframework/phoenix/blob/a106791ea9ee94b4f1e9286e45a319794b3838e2/lib/mix/tasks/phx.gen.release.ex#L308C1-L314C6
+  defp ensure_app!(app) do
+    if function_exported?(Mix, :ensure_application!, 1) do
+      apply(Mix, :ensure_application!, [app])
+    else
+      {:ok, _} = Application.ensure_all_started(app)
+    end
+  end
+
+  # https://github.com/phoenixframework/phoenix/blob/a106791ea9ee94b4f1e9286e45a319794b3838e2/lib/mix/tasks/phx.gen.release.ex#L353C1-L356C6
+  defp protocol_versions do
+    otp_major_vsn = :erlang.system_info(:otp_release) |> List.to_integer()
+    if otp_major_vsn < 25, do: [:"tlsv1.2"], else: [:"tlsv1.2", :"tlsv1.3"]
   end
 end
