98cdfaf0ddb401f1d1a006bba4b054d6a768d8ca
Alex Drummond <a.d.drummond@gmail.com>
2025-08-07 21:02:08 +0100

    add CMAKE_LD

--- js_server/TC-gcc-arm64.cmake
+++ js_server/TC-gcc-arm64.cmake
@@ -8,6 +8,7 @@ set(CMAKE_CROSSCOMPILING TRUE)
 SET(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 set(CMAKE_AR aarch64-linux-gnu-ar)
+set(CMAKE_LD aarch64-linux-gnu-ld)
 set(CMAKE_RANLIB aarch64-linux-gnu-ranlib)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
