405412bcb77ce086e8de2701be16a26b280528a4
Alex Drummond <a.d.drummond@gmail.com>
2025-08-09 10:37:27 +0100

    add missing call to JS_FreeCString

--- js_server/src/main.c
+++ js_server/src/main.c
@@ -689,6 +689,7 @@ static int handle_line_3_parameter(ThreadState *ts, const char *line, int len) {
   JS_FreeValue(ts->ctx, parsed_arg);
   JS_FreeValue(ts->ctx, ret);
 
+  JS_FreeCString(ts->ctx, str);
   JS_FreeValue(ts->ctx, stringified);
 
   if (0 == (ts->memory_check_count =
