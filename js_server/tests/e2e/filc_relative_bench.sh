e34c8500a013719ef7688891bf489704c70f9c85
Alex Drummond <a.d.drummond@gmail.com>
2025-08-10 10:12:31 +0100

    some more work on Fil-C bench

--- js_server/tests/e2e/filc_relative_bench.sh
+++ js_server/tests/e2e/filc_relative_bench.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+# ^ specifying bash for this one as we need the 'time' keyword
 
 set -e
+
+N_ITERATIONS=10000
 
 # TODO: complete
 exit 0
@@ -24,5 +27,35 @@ openssl pkey -inform pem -pubout -outform der -in filc_bench_private_signing_key
 export JSOCKD_BYTECODE_MODULE_PUBLIC_KEY=$(cat filc_bench_public_signing_key)
 
 ../tools-bin/compile_es6_module tests/e2e/filc_relative_bench/bundle.js tests/e2e/filc_relative_bench/bundle.qjsbc filc_bench_private_signing_key.pem
+
+# Start the server (regular x86_64 build)
+(
+    ./build_Release/js_server -m tests/e2e/filc_relative_bench/bundle.qjsbc -s /tmp/jsockd_filc_relative_bench_sock ;
+    echo $? > /tmp/jsockd_filc_relative_bench_regular_server_exit_code
+) &
+regular_server_pid=$!
+i=0
+while ! [ -e /tmp/jsockd_filc_relative_bench_sock] && [ $i -lt 15 ]; do
+  echo "Waiting for regular x86_64 server to start"
+  sleep 1
+  i=$(($i + 1))
+done
+sleep 1
+
+time (
+    i=0
+    while [ $i -lt $N_ITERATIONS ]; do
+    echo "Iteration $i"
+    # Run the client against the regular x86_64 server
+    nc -U /tmp/jsockd_filc_relative_bench_sock <<EOF
+    command_id
+    m => m.renderToString(m.createElement(m.AccordionDemo))
+    0
+    EOF
+    i=$(($i + 1))
+    done
+)
+
+echo ?quit > nc -U /tmp/jsockd_filc_relative_bench_sock
 
 # m => m.renderToString(m.createElement(m.AccordionDemo))
