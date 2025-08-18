67c9345a616143ff9eb7dadc7c1efa8a9422a60a
Alex Drummond <alex.drummond@multiverse.io>
2025-08-18 10:09:36 +0100

    remove accidental reformatting of library code

--- js_server/src/lib/ed25519/seed.c
+++ js_server/src/lib/ed25519/seed.c
@@ -3,42 +3,38 @@
 #ifndef ED25519_NO_SEED
 
 #ifdef _WIN32
-#include <wincrypt.h>
 #include <windows.h>
+#include <wincrypt.h>
 #else
 #include <stdio.h>
 #endif
 
 int ed25519_create_seed(unsigned char *seed) {
 #ifdef _WIN32
-  HCRYPTPROV prov;
+    HCRYPTPROV prov;
 
-  if (!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL,
-                           CRYPT_VERIFYCONTEXT)) {
-    return 1;
-  }
+    if (!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))  {
+        return 1;
+    }
 
-  if (!CryptGenRandom(prov, 32, seed)) {
+    if (!CryptGenRandom(prov, 32, seed))  {
+        CryptReleaseContext(prov, 0);
+        return 1;
+    }
+
     CryptReleaseContext(prov, 0);
-    return 1;
-  }
-
-  CryptReleaseContext(prov, 0);
-
-  return 0;
 #else
-  FILE *f = fopen("/dev/urandom", "rb");
+    FILE *f = fopen("/dev/urandom", "rb");
 
-  if (f == NULL) {
-    return 1;
-  }
+    if (f == NULL) {
+        return 1;
+    }
 
-  if (0 == fread(seed, 1, 32, f))
-    return -1;
-  if (0 != fclose(f))
-    return -1;
-  return 0;
+    fread(seed, 1, 32, f);
+    fclose(f);
 #endif
+
+    return 0;
 }
 
 #endif
