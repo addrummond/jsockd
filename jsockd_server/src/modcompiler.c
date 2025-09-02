b13e743cfed50184d2978a70d4dfe6d02b93e157
Alex Drummond <a.d.drummond@gmail.com>
2025-09-02 19:50:40 +0100

    correct comment

--- jsockd_server/src/modcompiler.c
+++ jsockd_server/src/modcompiler.c
@@ -14,7 +14,7 @@
 // The module file format:
 //     raw QuickJS bytecode
 //     128 byte version string, null-terminated
-//     64 byte ed25519 signature of bytecode + version string
+//     64 byte ed25519 signature of bytecode
 
 int compile_module_file(const char *module_filename, const char *key_file,
                         const char *output_filename) {
