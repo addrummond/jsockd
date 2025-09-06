91e0945be8849e74bd53b111c502610de98db2f0
Alex Drummond <a.d.drummond@gmail.com>
2025-09-06 19:26:00 +0100

    logic fix to line_buf_replay

--- jsockd_server/src/line_buf.c
+++ jsockd_server/src/line_buf.c
@@ -30,23 +30,22 @@ int line_buf_replay(LineBuf *b, char sep_char,
                     void *line_handler_data) {
   int i;
   int sep = -1;
-  for (i = b->start; i < b->start + b->n; ++i) {
+  int lh_r = 0;
+  for (i = b->start; i < b->start + b->n && lh_r >= 0; ++i) {
     if (b->buf[i] == sep_char) {
       b->buf[i] = '\0';
       // Note: the parens around (sep + 1) are necessary to avoid (temporarily)
       // decrementing b->buf to the position before its first byte, invoking the
       // spectre of undefined behavior (consider the case where sep == -1).
-      int lh_r = line_handler(b->buf + (sep + 1), i - sep - 1,
-                              line_handler_data, b->truncated);
+      lh_r = line_handler(b->buf + (sep + 1), i - sep - 1, line_handler_data,
+                          b->truncated);
       sep = i;
       b->truncated = false;
-      if (lh_r < 0)
-        return lh_r;
     }
   }
 
   memcpy(b->buf, b->buf + sep + 1, i - sep - 1);
   b->start = i - sep - 1;
 
-  return b->n;
+  return lh_r < 0 ? lh_r : b->n;
 }
