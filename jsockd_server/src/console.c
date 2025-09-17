c5494e04a03e2fafd777a3d4d56d91098cee9b04
Alex Drummond <a.d.drummond@gmail.com>
2025-09-17 19:07:01 +0100

    add console.c/h

---
+++ jsockd_server/src/console.c
@@ -0,0 +1,85 @@
+#include "quickjs.h"
+#include "utils.h"
+#include <stdatomic.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+extern pthread_mutex_t g_log_mutex;
+extern atomic_bool g_log_mutex_initialized;
+
+typedef struct {
+  const char *data;
+  size_t pos;
+} LogState;
+
+static size_t remove_trailing_ws(const char *buf, size_t len) {
+  if (len == 0)
+    return 0;
+  --len;
+  do {
+    if (buf[len] != ' ' && buf[len] != '\t' && buf[len] != '\n' &&
+        buf[len] != '\r')
+      break;
+  } while (len-- > 0);
+  return len + 1;
+}
+
+static void js_print_value_write(void *opaque, const char *buf, size_t len) {
+  FILE *fo = opaque;
+
+  len = remove_trailing_ws(buf, len);
+
+  int line = 1;
+  print_log_prefix(LOG_INFO, fo, line);
+  fputs("<console.*>: ", fo);
+  size_t start = 0;
+  size_t i;
+  for (i = 0; i < len; ++i) {
+    if (buf[i] == '\n') {
+      fwrite(buf + start, 1, i - start, fo);
+      fputc('\n', stderr);
+      ++line;
+      start = i + 1;
+      print_log_prefix(LOG_INFO, fo, line);
+    }
+  }
+  fwrite(buf + start, 1, i - start, fo);
+}
+
+static JSValue js_print(JSContext *ctx, JSValueConst this_val, int argc,
+                        JSValueConst *argv) {
+  int i;
+  JSValueConst v;
+
+  for (i = 0; i < argc; i++) {
+    if (i != 0)
+      fputc(' ', stderr);
+    v = argv[i];
+    if (JS_IsString(v)) {
+      const char *str;
+      size_t len;
+      str = JS_ToCStringLen(ctx, &len, v);
+      if (!str)
+        return JS_EXCEPTION;
+      js_print_value_write((void *)stderr, str, len);
+      JS_FreeCString(ctx, str);
+    } else {
+      JS_PrintValue(ctx, js_print_value_write, stderr, v, NULL);
+    }
+  }
+  fputc('\n', stderr);
+  return JS_UNDEFINED;
+}
+
+JSValue my_js_console_log(JSContext *ctx, JSValueConst this_val, int argc,
+                          JSValueConst *argv) {
+  if (g_log_mutex_initialized)
+    mutex_lock(&g_log_mutex);
+  JSValue ret;
+  ret = js_print(ctx, this_val, argc, argv);
+  fflush(stderr);
+  if (g_log_mutex_initialized)
+    mutex_unlock(&g_log_mutex);
+  return ret;
+}
