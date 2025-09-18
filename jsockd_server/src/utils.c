b467c718451185e8f4d68e92db13542c4e321724
Alex Drummond <alex.drummond@multiverse.io>
2025-09-18 14:47:40 +0100

    try explicit 0 init

--- jsockd_server/src/utils.c
+++ jsockd_server/src/utils.c
@@ -108,17 +108,19 @@ void release_logf(LogLevel log_level, const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
 
-  static char log_buf_[8192];
+  static char log_buf_[8192] = {0};
   char *log_buf = log_buf_;
+
+  int n = vsnprintf(NULL, 0, fmt, args);
 
   if (g_log_mutex_initialized)
     mutex_lock(&g_log_mutex);
 
-  int n = vsnprintf(NULL, 0, fmt, args);
   if ((size_t)n > sizeof(log_buf_) / sizeof(log_buf_[0])) {
     log_buf = (char *)calloc((size_t)n, sizeof(char));
     memset(log_buf, 0, (size_t)n);
   }
+
   vsnprintf(log_buf, n, fmt, args);
   print_log_prefix(LOG_INFO, stderr, 1);
   log_with_prefix_for_subsequent_lines(
