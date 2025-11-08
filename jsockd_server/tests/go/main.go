456b4ba2720cb303eaeffbc76ae7b9b4a4d16ed8
Alex Drummond <a.d.drummond@gmail.com>
2025-11-08 18:09:59 +0000

    weird issue in go test

--- jsockd_server/tests/go/main.go
+++ jsockd_server/tests/go/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	rand "math/rand/v2"
+
 	"github.com/addrummond/jsockd/clients/go/jsockdclient"
 )
 
@@ -17,8 +19,29 @@ func main() {
 		socketNames[i] = fmt.Sprintf("/tmp/jsockd.%d.sock", i)
 	}
 
-	client, err := jsockdclient.InitJSockDClient(config, os.Args[1], socketNames)
-	if client.Err != nil {
+	client, err := jsockdclient.InitJSockDClient(config, os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error initializing client: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
 
+	for i := 0; i < 100000; i++ {
+		n := rand.IntN(1000)
+		cmd := fmt.Sprintf("(_m, p) => p + %v", n)
+		fmt.Printf("Sending command %s with param %v\n", cmd, n)
+		resp, err := jsockdclient.SendCommand[int](client, cmd, n)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error sending command: %v\n", err)
+			os.Exit(1)
+		}
+		if resp.Exception {
+			fmt.Fprintf(os.Stderr, "Received exception: %v\n", resp.Exception)
+			os.Exit(1)
+		}
+		if resp.Result != 2*n {
+			fmt.Fprintf(os.Stderr, "Unexpected result for command (_m, p) => p + %v: got %v, expected %v\n", n, resp.Result, 2*n)
+			os.Exit(1)
+		}
 	}
 }
