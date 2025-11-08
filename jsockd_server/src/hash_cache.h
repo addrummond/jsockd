981082c5b59063bf044afe2a889d5c88f36cd038
Alex Drummond <a.d.drummond@gmail.com>
2025-11-08 19:30:35 +0000

    code formatting

--- jsockd_server/src/hash_cache.h
+++ jsockd_server/src/hash_cache.h
@@ -24,7 +24,7 @@ typedef struct {
 size_t get_cache_bucket(HashCacheUid uid, int n_bits);
 HashCacheUid get_hash_cache_uid(const void *data, size_t size);
 HashCacheBucket *get_hash_cache_bucket(HashCacheBucket buckets[], int n_bits,
-                                   HashCacheUid uid);
+                                       HashCacheUid uid);
 HashCacheBucket *get_hash_cache_entry(HashCacheBucket buckets[], int n_bits,
                                       HashCacheUid uid);
 
