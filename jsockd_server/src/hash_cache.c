9b16837b173fdcd9455f88c24df7e17790abc182
Alex Drummond <a.d.drummond@gmail.com>
2025-11-30 23:58:57 +0000

    temp

--- jsockd_server/src/hash_cache.c
+++ jsockd_server/src/hash_cache.c
@@ -39,10 +39,9 @@ HashCacheBucket *add_to_hash_cache_(HashCacheBucket *buckets,
     uint_fast64_t expected0uint64 = 0;
     int expected0int = 0;
 
-    // There's an empty bucket. In the initial atomic compare/exchange, we set
-    // its uid to 1, which will neither match any real uid nor register as
-    // 'empty', so that no other thread will attempt to read from or modify this
-    // bucket while we mess around with it.
+    atomic_fetch_add_explicit(&bucket->update_count, 1, memory_order_release);
+
+    // There's an empty bucket.
     if (atomic_compare_exchange_strong_explicit(&bucket->uid, &expected0uint64,
                                                 1, memory_order_acq_rel,
                                                 memory_order_acquire)) {
@@ -57,13 +56,14 @@ HashCacheBucket *add_to_hash_cache_(HashCacheBucket *buckets,
     if (atomic_compare_exchange_strong_explicit(
             &bucket->refcount, &expected0int, 1, memory_order_acq_rel,
             memory_order_acquire)) {
-      atomic_fetch_add_explicit(&bucket->update_count, 1, memory_order_release);
       if (cleanup)
         cleanup(bucket);
       memcpy((void *)((char *)bucket + object_offset), object, object_size);
       atomic_store_explicit(&bucket->uid, uid, memory_order_release);
       return bucket;
     }
+
+    atomic_fetch_add_explicit(&bucket->update_count, -1, memory_order_release);
   }
   return NULL;
 }
@@ -83,16 +83,19 @@ HashCacheBucket *get_hash_cache_entry_(HashCacheBucket *buckets,
       int update_count_before =
           atomic_load_explicit(&bucket->update_count, memory_order_acquire);
 
-      if (atomic_load_explicit(&bucket->uid, memory_order_acquire) != uid)
+      if (uid != atomic_load_explicit(&bucket->uid, memory_order_acquire))
         break;
 
       atomic_fetch_add_explicit(&bucket->refcount, 1, memory_order_release);
 
-      if (atomic_load_explicit(&bucket->update_count, memory_order_acquire) !=
-          update_count_before)
-        continue; // bucket was modified while we were raeding it; try again
+      bool updated =
+          update_count_before !=
+          atomic_load_explicit(&bucket->update_count, memory_order_acquire);
 
       atomic_fetch_add_explicit(&bucket->refcount, -1, memory_order_release);
+
+      if (updated)
+        continue;
 
       return bucket;
     }
