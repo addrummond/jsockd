ee2a94e0fc9e58021fb30997c76f9513101622ad
Alex Drummond <a.d.drummond@gmail.com>
2025-12-12 20:51:18 +0000

    uncomment valgrind tests

--- jsockd_server/tests/valgrind_eval/run.sh
+++ jsockd_server/tests/valgrind_eval/run.sh
@@ -16,14 +16,14 @@ export JSOCKD_BYTECODE_MODULE_PUBLIC_KEY=$(cat valgrind_private_signing_key.pubk
 
 JSOCKD="valgrind --error-exitcode=1 --leak-check=full --track-origins=yes -- ./build_Debug/jsockd"
 
-#$JSOCKD -e '(() => { return { foo: "bar" }; })()'
-#$JSOCKD -m /tmp/bundle.qjsb -e '(() => { return { foo: "bar" }; })()'
-#$JSOCKD -m /tmp/bundle.qjsb -e 'M.getAValue()';
-#$JSOCKD -m /tmp/bundle.qjsb -e '(() => { return { foo: "bar" }; })()' -sm tests/valgrind_server/bundle.mjs.map
-#$JSOCKD -m /tmp/bundle.qjsb -e 'M.getAValue()' -sm tests/valgrind_server/bundle.mjs.map
+$JSOCKD -e '(() => { return { foo: "bar" }; })()'
+$JSOCKD -m /tmp/bundle.qjsb -e '(() => { return { foo: "bar" }; })()'
+$JSOCKD -m /tmp/bundle.qjsb -e 'M.getAValue()';
+$JSOCKD -m /tmp/bundle.qjsb -e '(() => { return { foo: "bar" }; })()' -sm tests/valgrind_server/bundle.mjs.map
+$JSOCKD -m /tmp/bundle.qjsb -e 'M.getAValue()' -sm tests/valgrind_server/bundle.mjs.map
 
-#echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -e - < /tmp/valgrind_eval_test_input
-#echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
-#echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
+echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -e - < /tmp/valgrind_eval_test_input
+echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
+echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
 echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - -sm tests/valgrind_server/bundle.mjs.map < /tmp/valgrind_eval_test_input
-#echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - -sm tests/valgrind_server/bundle.mjs.map < /tmp/valgrind_eval_test_input
+echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - -sm tests/valgrind_server/bundle.mjs.map < /tmp/valgrind_eval_test_input
