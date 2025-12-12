313711288408163f4afb057d1dd9857e943886e4
Alex Drummond <a.d.drummond@gmail.com>
2025-12-12 19:36:09 +0000

    temp

--- jsockd_server/tests/valgrind_eval/run.sh
+++ jsockd_server/tests/valgrind_eval/run.sh
@@ -22,8 +22,8 @@ $JSOCKD -m /tmp/bundle.qjsb -e 'M.getAValue()';
 $JSOCKD -m /tmp/bundle.qjsb -e '(() => { return { foo: "bar" }; })()' -sm tests/valgrind_server/bundle.mjs.map
 $JSOCKD -m /tmp/bundle.qjsb -e 'M.getAValue()' -sm tests/valgrind_server/bundle.mjs.map
 
-#echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -e - < /tmp/valgrind_eval_test_input
-#echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
-#echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
-#echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ;  $JSOCKD -m /tmp/bundle.qjsb -e - -sm tests/valgrind_server/bundle.mjs.map < /tmp/valgrind_eval_test_input
-#echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - -sm tests/valgrind_server/bundle.mjs.map < /tmp/valgrind_eval_test_input
+echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -e - < /tmp/valgrind_eval_test_input
+echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
+echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - < /tmp/valgrind_eval_test_input
+echo '(() => { return { foo: "bar" }; })()' > /tmp/valgrind_eval_test_input ;  $JSOCKD -m /tmp/bundle.qjsb -e - -sm tests/valgrind_server/bundle.mjs.map < /tmp/valgrind_eval_test_input
+echo 'M.getAValue()' > /tmp/valgrind_eval_test_input ; $JSOCKD -m /tmp/bundle.qjsb -e - -sm tests/valgrind_server/bundle.mjs.map < /tmp/valgrind_eval_test_input
