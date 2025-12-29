c60fb08b6dfd3bb9ced0cb48e502a565377b32ee
Alex Drummond <a.d.drummond@gmail.com>
2025-12-29 13:05:10 +0000

    v bump

--- clients/go/jsockdclient/jsockd.go
+++ clients/go/jsockdclient/jsockd.go
@@ -49,7 +49,7 @@ import (
 )
 
 // JSockDVersion specifies the expected version of JSockD [__jsockd_version_check__ <- for automatic CI check]
-const JSockDVersion = "0.0.136"
+const JSockDVersion = "0.0.137"
 
 const chanBufferSize = 64
 
