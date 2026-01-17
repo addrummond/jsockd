b53976a3bc25ac307b8d570d46d0271614212b2b
Alex Drummond <a.d.drummond@gmail.com>
2026-01-17 19:53:18 +0000

    temp

--- build_quickjs.sh
+++ build_quickjs.sh
@@ -134,10 +134,10 @@ for platform in $platforms; do
             export CL_LDFLAGS='/LIBPATH:pthreads-win32-lib pthreadVC2.lib'
             # remove unwanted targets from Makefile
             sed 's/^\(PROGS=.*\) run-test262\$(EXE)\(.*\)$/\1\2/' Makefile > output.mk && mv output.mk Makefile
-            echo "*** RUNNING make ***"
             WPWD=$(cygpath -w $(dirname $(dirname "$PWD")))
+            echo "*** RUNNING make ***"
             # shellcheck disable=SC2211
-            /c/Program\ Files\ */GnuWin32/bin/make.exe CC=../../win-build-utils/fakecl.sh HOST_CC=../../win-build-utils/fakecl.sh AR=../../win-build-utils/fakear.sh CFLAGS="/FI $WPWD\\builtins.h /O2 /std:c17 /experimental:c11atomics -Ipthreads-win32-include -D_WINSOCKAPI_ -DWIN32_LEAN_AND_MEAN"
+            /c/Program\ Files\ */GnuWin32/bin/make.exe CC=../../win-build-utils/fakecl.sh HOST_CC=../../win-build-utils/fakecl.sh AR=../../win-build-utils/fakear.sh CFLAGS="/FI $WPWD\\win-build-utils\\builtins.h /O2 /std:c17 /experimental:c11atomics -Ipthreads-win32-include -D_WINSOCKAPI_ -DWIN32_LEAN_AND_MEAN"
             echo "*** BUILD COMPLETE ***"
             ;;
         mac_arm64)
