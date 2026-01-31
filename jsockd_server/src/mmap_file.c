4901726a8c2b8bc268979bf53517b7128e9ae46c
Alex Drummond <a.d.drummond@gmail.com>
2026-01-31 21:13:33 +0000

    fix exceeds_size_max logic

--- jsockd_server/src/mmap_file.c
+++ jsockd_server/src/mmap_file.c
@@ -1,12 +1,18 @@
 #include "mmap_file.h"
-#include "typeofshim.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+
+static bool exceeds_size_max(off_t x) {
+  const off_t cmp = (off_t)(SIZE_MAX);
+  return cmp > 0 && x > cmp;
+}
 
 const uint8_t *mmap_file(const char *filename, size_t *out_size,
                          int *out_errno) {
@@ -22,8 +28,7 @@ const uint8_t *mmap_file(const char *filename, size_t *out_size,
     close(fd);
     return NULL;
   }
-  if (!S_ISREG(st.st_mode) || st.st_size <= 0 ||
-      st.st_size > ((TYPEOF(st.st_size))(SIZE_MAX))) {
+  if (!S_ISREG(st.st_mode) || st.st_size <= 0 || exceeds_size_max(st.st_size)) {
     *out_errno = EINVAL;
     close(fd);
     return NULL;
