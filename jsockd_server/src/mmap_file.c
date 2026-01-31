248be08cdb6d7c4f3cca1e1717f6274ebdf940af
Alex Drummond <a.d.drummond@gmail.com>
2026-01-31 19:43:29 +0000

    improvements to mmap_file

--- jsockd_server/src/mmap_file.c
+++ jsockd_server/src/mmap_file.c
@@ -1,6 +1,7 @@
 #include "mmap_file.h"
 #include <errno.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -15,23 +16,24 @@ const uint8_t *mmap_file(const char *filename, size_t *out_size,
     return NULL;
   }
   struct stat st;
-  if (0 != fstat(fd, &st) || st.st_size == 0) {
+  if (0 != fstat(fd, &st)) {
     *out_errno = errno;
     close(fd);
     return NULL;
   }
-  *out_size = st.st_size;
-  const uint8_t *contents =
-      (const uint8_t *)mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (!S_ISREG(st.st_mode) || st.st_size <= 0 || st.st_size > SIZE_MAX) {
+    *out_errno = EINVAL;
+    close(fd);
+    return NULL;
+  }
+  *out_size = (size_t)st.st_size;
+  const uint8_t *contents = (const uint8_t *)mmap(
+      NULL, (size_t)st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   if (contents == MAP_FAILED) {
     *out_errno = errno;
     close(fd);
     return NULL;
   }
-  if (0 != close(fd)) {
-    *out_errno = errno;
-    munmap((void *)contents, st.st_size);
-    return NULL;
-  }
+  close(fd);
   return contents;
 }
