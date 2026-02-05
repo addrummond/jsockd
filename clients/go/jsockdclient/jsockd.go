d0c0df670d3c6996a98ce508083463ca66d11f0a
Alex Drummond <alex.drummond@multiverse.io>
2026-02-05 17:01:19 +0000

    v bump

--- clients/go/jsockdclient/jsockd.go
+++ clients/go/jsockdclient/jsockd.go
@@ -49,7 +49,7 @@ import (
 )
 
 // JSockDVersion specifies the expected version of JSockD [__jsockd_version_check__ <- for automatic CI check]
-const JSockDVersion = "0.0.144"
+const JSockDVersion = "0.0.145"
 
 const chanBufferSize = 64
 
--- docs/ssr_with_react_19_example_code/go.mod
+++ docs/ssr_with_react_19_example_code/go.mod
@@ -3,4 +3,4 @@ module react-ssr-example
 go 1.25.1
 
 // __jsockd_version_check__
-require github.com/addrummond/jsockd/clients/go/jsockdclient v0.0.144
+require github.com/addrummond/jsockd/clients/go/jsockdclient v0.0.145
