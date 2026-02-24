08e2a3a378c15b908805e958c0d852b8fbf6b289
Alex Drummond <a.d.drummond@gmail.com>
2026-02-24 20:38:25 +0000

    beef up compile time atomicity checks

--- jsockd_server/src/hash_cache.h
+++ jsockd_server/src/hash_cache.h
@@ -52,6 +52,13 @@ typedef struct {
 // used).
 _Static_assert(sizeof(HashCacheBucket) % 16 == 0,
                "HashCacheBucket must be multiple of 16 bytes in size");
+// Clang on AArch64 does not define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_16 despite
+// full support for 128-bit atomics (https://github.com/llvm/llvm-project/issues/71883).
+// We skip the check in that case.
+#if !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_16) && \
+    !(defined(__clang__) && (defined(__aarch64__) || defined(__arm64__)))
+#error "compiler does not support lock-free 128-bit atomics"
+#endif
 #endif
 
 #define HASH_CACHE_BUCKET_ARRAY_SIZE_FROM_HASH_BITS(hash_bits)                 \
